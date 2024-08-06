82f2fdd6b6e5aba7d2d8c4184955418f65dfc278
Matias Scherer Lunkes <mslunkes@outlook.com>
2024-08-05 21:06:43 -0300

    correção do css

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-$(document).ready(function(){
-    $('header button').click(function(){
+
+$(document).ready(function() {
+    $('header button').click(function() {
         $('form').slideDown();
     })
-    $('#botao-cancelar').click(function(){
+
+    $('#botao-cancelar').click(function() {
         $('form').slideUp();
     })
 
-    $('form').on('submit', function(e){
+    $('form').on('submit', function(e) {
         e.preventDefault();
-        const endereçoDaNovaImagem = $('#endereco-imagem-nova').val()
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
         const novoItem = $('<li style="display: none"></li>');
-        $(`<img src="${endereçoDaNovaImagem}"/>`).appendTo(novoItem)
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(`
-            <div class="overlay-imagem-link>
-                <a href="${endereçoDaNovaImagem} target="_blank" title="Ver imagem no tamanho real">
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
                     Ver imagem em tamanho real
                 </a>
             </div>
         `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn();
+        $(novoItem).fadeIn(1000);
         $('#endereco-imagem-nova').val('')
     })
 })
